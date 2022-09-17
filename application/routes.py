325f1942b6574e0f4fb85ee38a85c87eff499943
Helen Guan <helenguan30@gmail.com>
2022-09-17 19:49:50 -0400

    front end for forms

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,5 @@
-from symbol import yield_arg
 from application import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from datetime import date
 
 @app.route('/')
@@ -8,6 +7,17 @@ from datetime import date
 def home():
     return render_template('home.html', home = True, time = date.today())
 
-@app.route('/settings')
+@app.route('/settings', methods=['GET', 'POST'])
 def settings():
-    return render_template('settings.html', settings = True)
+    pill1Name = request.form.get('pill1Name')
+    print(pill1Name)
+    return render_template("settings.html", settings=True, pill1Name=pill1Name)
+    
+
+@app.route('/pill1')
+def pill1():
+    if request.method == 'POST': 
+        pill1Name = request.form["pill1Name"]
+        return redirect(url_for('settings',settings=True, pill1Name=pill1Name))
+    return render_template('pill1.html')
+    
